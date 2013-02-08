6173f0fcd3da398b93efe721ad81c5d507d71afb
anthem001 <anthem001@gmail.com>
2013-02-07 19:17:46 -0700

    initial commit

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* instantiate entity object */
+            AdventureWorksLT2008R2Entities entities = new AdventureWorksLT2008R2Entities();
+
+            /* LINQ query */
+            IEnumerable<Customer> customers = from c in entities.Customers
+                            select c;
+
+            /* iterate over enumerable */
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", customer.Title, customer.FirstName, customer.LastName, customer.Suffix);
+            }
+
+            /* pause to view output */
+            Console.ReadLine();
+
+        }
+    }
+}
